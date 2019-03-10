b8479e374f95597c7809160ef04022d3fcabc830
pfg <omegariisthebest@gmail.com>
2019-03-09 20:50:58 -0800

    Initial 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {parse} from "scpl";
+import logo from "./logo.svg";
+import "./App.css";
+
+import brace from "brace";
+import AceEditor from "react-ace";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<AceEditor
+					mode="javascript"
+					theme="github"
+					onChange={this.onChange.bind(this)}
+					name="ace_editor"
+					editorProps={{$blockScrolling: true}}
+				/>
+			</div>
+		);
+	}
+	onChange() {
+		
+	}
 }
 
 export default App;
