be25f1d913581348bf4ac947bc3a728b3e408b56
pfg <omegariisthebest@gmail.com>
2019-03-11 17:03:15 -0700

    Publish github pages site

---
+++ docs/precache-manifest.b0c897827a75ef4a9eafc8648298c227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a9ddcf7db9cbdebaf58",
+    "url": "/static/css/main.fe76b76f.chunk.css"
+  },
+  {
+    "revision": "1a9ddcf7db9cbdebaf58",
+    "url": "/static/js/main.4f0b80af.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2ec5701dfee85cbb382a",
+    "url": "/static/css/2.4cf574c6.chunk.css"
+  },
+  {
+    "revision": "2ec5701dfee85cbb382a",
+    "url": "/static/js/2.25bf00ef.chunk.js"
+  },
+  {
+    "revision": "be52308972b718a8aa416a95070ee376",
+    "url": "/static/media/icons@2x.be523089.png"
+  },
+  {
+    "revision": "88982dbdbb218298d2a3f7cc55f5950f",
+    "url": "/index.html"
+  }
+];
