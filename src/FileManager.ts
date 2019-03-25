099fddf77f9191a488751905698f6a811c28ee95
pfg <omegariisthebest@gmail.com>
2019-03-24 21:34:44 -0700

    Start on file manager

---
+++ src/FileManager.ts
@@ -0,0 +1,20 @@
+export class FileManager {
+	constructor() {
+		
+	}
+	listItems() { // returns a list of files and folders
+		
+	}
+	createFolder() {
+		
+	}
+	saveFile(path: string, data: string) { // saves the file at the path
+		
+	}
+	loadFile(path: string, data: string) {
+		
+	}
+	renameFile(path: string, newPath: string) {
+		
+	}
+}
