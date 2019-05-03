8578b19009cca9e557007f33c34351fbe003fade
pfg <omegariisthebest@gmail.com>
2019-05-03 07:25:06 -0700

    Improve DownloadModal.tsx

--- src/DownloadModal.tsx
+++ src/DownloadModal.tsx
@@ -10,6 +10,94 @@ import { uploadShortcut } from "./Uploader";
 
 import "./CreateEditShortcut.css";
 
+export class ShortcutDownloadPreviewButton extends Component<
+	{ filename: string; filesize: number },
+	{}
+> {
+	render() {
+		return (
+			<div>
+				<img src={shortcutDownloadPreviewIcon} width={130} />
+				<div className="shortcut-filename">{this.props.filename}</div>
+				<div className="shortcut-filedetails">
+					{prettyBytes(this.props.filesize)}
+				</div>
+			</div>
+		);
+	}
+}
+
+type UploadStatus =
+	| {
+			uploadStatus: "None" | "Uploading";
+	  }
+	| {
+			uploadStatus: "URL";
+			uploadedURL: string;
+	  }
+	| {
+			uploadStatus: "Error";
+			uploadError: string;
+	  };
+
+export class ShortcutDownloadStatusSwitcher extends Component<{
+	status: UploadStatus;
+	requestUpload: () => void;
+	uploadReason: string;
+	uploadAction: string;
+}> {
+	render() {
+		switch (this.props.status.uploadStatus) {
+			case "None":
+				return (
+					<div>
+						<p>
+							To {this.props.uploadReason}, ScPL editor will
+							upload your shortcut to{" "}
+							<a
+								href="https://file.io"
+								target="_blank"
+								rel="noopener"
+							>
+								file.io
+							</a>
+							.
+						</p>
+						<button
+							className="large-btn"
+							onClick={() => this.props.requestUpload()}
+						>
+							{this.props.uploadAction}
+						</button>
+					</div>
+				);
+			case "URL":
+				return this.props.children;
+			case "Uploading":
+				return (
+					<div>
+						<div className="generate-code-load">
+							<div className="load" />
+							<p>Generating QR Code...</p>
+						</div>
+					</div>
+				);
+			case "Error":
+				return (
+					<div>
+						<p>Error uploading: {this.props.status.uploadError}</p>
+					</div>
+				);
+			default:
+				return (
+					<div>
+						<p>Something bad happened.</p>
+					</div>
+				);
+		}
+	}
+}
+
 type DownloadModalProps = {
 	onCancel: () => void;
 	filename: string;
@@ -86,212 +174,71 @@ export class DownloadModal extends Component<
 										filename={this.props.filename}
 										file={this.props.file}
 									>
-										<img
-											src={shortcutDownloadPreviewIcon}
-											width={130}
-										/>
-										<div className="shortcut-filename">
-											{this.props.filename}
-										</div>
-										<div className="shortcut-filedetails">
-											{prettyBytes(
-												Buffer.byteLength(
-													this.props.file
-												)
+										<ShortcutDownloadPreviewButton
+											filename={this.props.filename}
+											filesize={Buffer.byteLength(
+												this.props.file
 											)}
-										</div>
+										/>
 									</DownloadButton>
 								) : (
-									(() => {
-										switch (this.state.uploadStatus) {
-											case "None":
-												return (
-													<div>
-														<p>
-															To download on this
-															browser, ScPL editor
-															will upload your
-															shortcut to{" "}
-															<a
-																href="https://file.io"
-																target="_blank"
-																rel="noopener"
-															>
-																file.io
-															</a>
-															.
-														</p>
-														<button
-															className="large-btn"
-															onClick={() =>
-																this.uploadFile()
-															}
-														>
-															Download
-														</button>
-													</div>
-												);
-											case "URL":
-												return (
-													<a
-														target="_blank"
-														rel="noopener noreferrer"
-														href={
-															this.state
-																.uploadedURL
-														}
-													>
-														<img
-															src={
-																shortcutDownloadPreviewIcon
-															}
-															width={130}
-														/>
-														<div className="shortcut-filename">
-															{
-																this.props
-																	.filename
-															}
-														</div>
-														<div className="shortcut-filedetails">
-															{prettyBytes(
-																Buffer.byteLength(
-																	this.props
-																		.file
-																)
-															)}
-														</div>
-													</a>
-												);
-											case "Uploading":
-												return (
-													<div>
-														<div className="generate-code-load">
-															<div className="load" />
-															<p>Uploading</p>
-														</div>
-													</div>
-												);
-											case "Error":
-												return (
-													<div>
-														<p>
-															Error uploading:{" "}
-															{
-																this.state
-																	.uploadError
-															}
-														</p>
-													</div>
-												);
-											default:
-												return (
-													<div>
-														<p>
-															Something bad
-															happened.
-														</p>
-													</div>
-												);
-										}
-									})()
+									<ShortcutDownloadStatusSwitcher
+										status={this.state}
+										requestUpload={() => this.uploadFile()}
+										uploadReason="download on this browser"
+										uploadAction="Create Download Link"
+									>
+										<a
+											target="_blank"
+											rel="noopener noreferrer"
+											href={
+												(this.state as any).uploadedURL
+											}
+										>
+											<ShortcutDownloadPreviewButton
+												filename={this.props.filename}
+												filesize={Buffer.byteLength(
+													this.props.file
+												)}
+											/>
+										</a>
+										);
+									</ShortcutDownloadStatusSwitcher>
 								)}
 							</div>
 							<div>
-								{(() => {
-									switch (this.state.uploadStatus) {
-										case "None":
-											return (
-												<div>
-													<p>
-														To generate a QR code,
-														ScPL editor will upload
-														your shortcut to{" "}
-														<a
-															href="https://file.io"
-															target="_blank"
-															rel="noopener"
-														>
-															file.io
-														</a>
-														.
-													</p>
-													<button
-														className="large-btn"
-														onClick={() =>
-															this.uploadFile()
-														}
-													>
-														Generate Code
-													</button>
-												</div>
-											);
-										case "URL":
-											return (
-												<div>
-													<p>
-														Add to your library via
-														QR Code:
-													</p>
-													<div id="qr-result">
-														<img
-															src={`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
-																this.state
-																	.uploadedURL
-															)}`}
-														/>
-													</div>
-													<p className="details-text">
-														Open your Camera app and
-														point it steady for 2-3
-														seconds at this QR Code.
-														<br />
-														<br />
-														If nothing happens, QR
-														Code scanning may not be
-														enabled on your device.
-													</p>
-												</div>
-											);
-										case "Uploading":
-											return (
-												<div>
-													<div className="generate-code-load">
-														<div className="load" />
-														<p>
-															Generating QR
-															Code...
-														</p>
-													</div>
-												</div>
-											);
-										case "Error":
-											return (
-												<div>
-													<p>
-														Error uploading:{" "}
-														{this.state.uploadError}
-													</p>
-												</div>
-											);
-										default:
-											return (
-												<div>
-													<p>
-														Something bad happened.
-													</p>
-												</div>
-											);
-									}
-								})()}
+								<ShortcutDownloadStatusSwitcher
+									status={this.state}
+									requestUpload={() => this.uploadFile()}
+									uploadReason="generate a QR code"
+									uploadAction="Generate QR Code"
+								>
+									<div>
+										<p>Add to your library via QR Code:</p>
+										<div id="qr-result">
+											<img
+												src={`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
+													(this.state as any)
+														.uploadedURL
+												)}`}
+											/>
+										</div>
+										<p className="details-text">
+											Open your Camera app and point it
+											steady for 2-3 seconds at this QR
+											Code.
+											<br />
+											<br />
+											If nothing happens, QR Code scanning
+											may not be enabled on your device.
+										</p>
+									</div>
+								</ShortcutDownloadStatusSwitcher>
 							</div>
 						</div>
 					) : (
 						<div id="no-file-msg">
-							<p>
-								Please write some actions to be converted into a
-								shortcut.
-							</p>
+							<p>Your shortcut may have some errors.</p>
 						</div>
 					)}
 					<div
