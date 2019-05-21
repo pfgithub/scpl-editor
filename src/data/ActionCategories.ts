5b85586d1f68db263b9952e99a91f2292fae2561
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-21 14:48:27 -0400

    Most of the search action icons are added

--- src/data/ActionCategories.ts
+++ src/data/ActionCategories.ts
@@ -3,5 +3,10 @@ export const categories: {[key: string]: string | undefined} = {
 	"Scripting": "action-item-gear",
 	"Calendar": "action-item-calendar",
 	"Documents": "action-item-files",
-	"Music": "action-item-music"
+	"Music": "action-item-music",
+	"Sharing": "action-item-sharing",
+	"Text": "action-item-text",
+	"Web": "action-item-web",
+	"Contacts": "action-item-contacts",
+	"Location": "action-item-location"
 };
