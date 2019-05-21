a88dfdc5b9f5dd11c6a107bd14607a65ea515321
internetgho5t <brandon_jordan360@yahoo.com>
2019-05-21 13:12:35 -0400

    New loading indicator

--- src/App.tsx
+++ src/App.tsx
@@ -621,7 +621,20 @@ OpenURLs`
 						) : null}
 						<div className="loading-result-progress">
 							<div>
-								<div className="load" />
+							<div className="spinner">
+<div className="bar1"></div>
+<div className="bar2"></div>
+<div className="bar3"></div>
+<div className="bar4"></div>
+<div className="bar5"></div>
+<div className="bar6"></div>
+<div className="bar7"></div>
+<div className="bar8"></div>
+<div className="bar9"></div>
+<div className="bar10"></div>
+<div className="bar11"></div>
+<div className="bar12"></div>
+</div>
 							</div>
 						</div>
 						{!this.state.showPreview ? (
