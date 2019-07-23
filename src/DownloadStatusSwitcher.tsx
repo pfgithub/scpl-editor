83bac56554b2edbc630530fd1824aeffe7f7f620
internetgho5t <brandon_jordan360@yahoo.com>
2019-07-23 11:52:17 -0400

    Update DownloadStatusSwitcher.tsx

--- src/DownloadStatusSwitcher.tsx
+++ src/DownloadStatusSwitcher.tsx
@@ -43,7 +43,20 @@ export class DownloadStatusSwitcher extends Component<{
 				return (
 					<div>
 						<div className="generate-code-load">
-							<div className="load" />
+						<div className="spinner">
+							<div className="bar1" />
+							<div className="bar2" />
+							<div className="bar3" />
+							<div className="bar4" />
+							<div className="bar5" />
+							<div className="bar6" />
+							<div className="bar7" />
+							<div className="bar8" />
+							<div className="bar9" />
+							<div className="bar10" />
+							<div className="bar11" />
+							<div className="bar12" />
+						</div>
 							<p>Uploading...</p>
 						</div>
 					</div>
